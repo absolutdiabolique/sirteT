30206bb00b185762c2e21656244fea0ab105a593
absolutdiabolique <personal.ronchen@gmail.com>
2026-06-04 10:49:04 -0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -12,9 +12,11 @@ const FIREBASE_CONFIG = {
 };
 
 export let db = null;
+export let auth = null;
 try {
   const app = initializeApp(FIREBASE_CONFIG);
   db = getDatabase(app);
+  auth = getAuth(app);
 } catch(e) {
   console.warn('Firebase init failed:', e.message);
 }
